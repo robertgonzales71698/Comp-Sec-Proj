bf068a491e4beecc08448a7a63decc5e97db0eb9
Robert Gonzales <robert.m.gonzales-1@ou.edu>
2021-11-04 23:44:57 -0500

    Initial commit

---
+++ PhishingDetector.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ * @author Gabriela Fisher, Isaigh Pugh, & Robert Gonzales
+ * @version Fall 2021
+ * The University of Oklahoma CS 4173 - Computer Securtiy
+ * 
+ * 
+ * 
+ */
+public class PhishingDetector
+{
+     public static void main() 
+     {
+          
+     }
+}
